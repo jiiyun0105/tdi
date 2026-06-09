e9a829eae558b8ba6f97deddc40105338791ba37
jiiyun0105 <jiiyun0105@gmail.com>
2026-06-10 00:56:04 +0900

    Final update for club and firebase

--- firebase.js
+++ firebase.js
@@ -1,13 +1,14 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-aLd3Qz0xV7fWf-P7K1C-g_oJ5L4Yc4k",
+  apiKey: "AIzaSyD1zkSEW7zoaf8dqIu7tAWg-zt...", // 실제 복사한 전체 내용을 넣으세요
   authDomain: "tdi-dongil-portal.firebaseapp.com",
   projectId: "tdi-dongil-portal",
   storageBucket: "tdi-dongil-portal.appspot.com",
-  messagingSenderId: "567385202684",
-  appId: "1:567385202684:web:0cf46876c666f284560a67"
+  messagingSenderId: "197229565116",
+  appId: "1:197229565116:web:aace10c5e584bee886ecb9"
 };
 
 const app = initializeApp(firebaseConfig);
